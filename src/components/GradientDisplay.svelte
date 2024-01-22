96e295e5da78db8e63f9f743d67789c767c0da54
zenDev-2 <cfk102@gmail.com>
2024-01-21 23:58:57 -0500

    build: gradient components and style reworks

---
+++ src/components/GradientDisplay.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  import GradientGridItem from "./GradientGridItem.svelte";
+  const gradientObjects = [
+    {
+      color1: "#000E1B",
+      color2: "#091C2E",
+      color3: "#D6A994",
+      gradientColor: "linear-gradient(to bottom, #000e1b, #091C2E, #d6a994)",
+    },
+    {
+      color1: "#000B00",
+      color2: "#4A5885",
+      color3: "#FBFAF5",
+      gradientColor: "linear-gradient(to bottom, #010101, #495883, #D3D0CB)",
+    },
+    {
+      color1: "#222940",
+      color2: "#D32F30",
+      color3: "#B3B2AE",
+      gradientColor: "linear-gradient(to bottom, #222940, #D32F30, #B3B2AE)",
+    },
+    {
+      color1: "#180614",
+      color2: "#C099A0",
+      color3: "#E4D2D8",
+      gradientColor: "linear-gradient(to bottom, #180614, #C099A0, #E4D2D8)",
+    },
+    {
+      color1: "#16160E",
+      color2: "#6C848D",
+      color3: "#E6D190",
+      gradientColor: "linear-gradient(to bottom, #16160E, #6C848D, #E6D190)",
+    },
+  ];
+</script>
+
+<div class="grid-container">
+  {#each gradientObjects as go}
+    <GradientGridItem props={go} />
+  {/each}
+</div>
+
+<style lang="scss">
+  .grid-container {
+    display: grid;
+
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    place-items: center;
+    gap: 2rem;
+  }
+</style>
---
+++ src/components/GradientGridItem.svelte
@@ -0,0 +1,72 @@
+<script>
+  export let props = {
+    color1: "",
+    color2: "",
+    color3: "",
+    gradientColor: "",
+  };
+</script>
+
+<div class="grid-item">
+  <div
+    class="gradient-circle"
+    style={`background-image: ${props.gradientColor}`}
+  ></div>
+
+  <div class="color-code--grid">
+    <div class="colorAndCode">
+      <div
+        class="color-circle color1"
+        style={`background-color: ${props.color1};`}
+      ></div>
+      <p>{props.color1}</p>
+    </div>
+
+    <div class="colorAndCode">
+      <div
+        class="color-circle color2"
+        style={`background-color: ${props.color2};`}
+      ></div>
+      <p>{props.color2}</p>
+    </div>
+
+    <div class="colorAndCode">
+      <div
+        class="color-circle color3"
+        style={`background-color: ${props.color3};`}
+      ></div>
+      <p>{props.color3}</p>
+    </div>
+  </div>
+</div>
+
+<style lang="scss">
+  .grid-item {
+    padding-top: 1.5em;
+  }
+  .gradient-circle {
+    margin: 0 auto;
+    width: clamp(70px, 14vw, 120px);
+    height: clamp(70px, 14vw, 120px);
+    border-radius: 50%;
+    background-size: contain;
+  }
+
+  .color-code--grid {
+    display: grid;
+    place-items: center;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 1.5rem;
+
+    .colorAndCode {
+      margin-top: 1.5rem;
+    }
+  }
+
+  .color-circle {
+    width: clamp(25px, 6.5vw, 45px);
+    height: clamp(25px, 6.5vw, 45px);
+    border-radius: 50%;
+    margin: 0 auto;
+  }
+</style>
